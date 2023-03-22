f33a2b69717b7d57453e50f18a280d8ab59dcd07
Dziero <88860699+Dziero@users.noreply.github.com>
2023-03-22 02:19:21 +0100

    task list

--- server.js
+++ server.js
@@ -14,19 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'public/pictures');
-  },
-  filename: function(req, file, cb) {
-    const ext = path.extname(file.originalname);
-    const filename = req.user ? req.user.id : 'unknown';
-    cb(null, filename + ext);
-  }
-});
-
-const upload = multer({ storage: storage });
-
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
@@ -66,10 +53,7 @@ app.post("/login", (req, res) => {
         req.session.user = username;
         if (result[0].admin === 0) {
           req.session.isAdmin = false;
-          res.render("dashboard", { 
-            name: req.session.user, 
-            perms: 'User'
-          });
+          res.redirect("dashboard"); 
         } 
         else if (result[0].admin === 1) {
           req.session.isAdmin = true;
@@ -151,30 +135,113 @@ app.get("/admin", (req, res) => {
   app.post('/users/:id/editor/delete', (req, res) => {
     const id = req.params.id;
     connection.query('DELETE FROM users WHERE id = ?', [id], () => {
-      res.redirect(`/users`);
+      res.redirect('/users');
     });
   });
   
 
-
-app.get("/logout", (req, res) => {
-  req.session.logged = false;
-  req.session.user = null;
-  req.session.isAdmin = false;
-  return res.redirect("/");
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'public/pictures');
+  },
+  filename: function(req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const filename = req.session.user_id ? req.session.user_id : 'unknown';
+    cb(null, filename + ext);
+  }
 });
 
+const upload = multer({ storage: storage });
 
-app.post('/users', upload.single('picture'), (req, res) => {
+app.post('/users', upload.single('picture') || function(req, res, next) { next(); }, (req, res) => {
+  if (!req.session.logged)  return res.redirect("/");
+  if (!req.session.isAdmin) return res.redirect("/");
+
   const { username, password } = req.body;
   if (!username || !password) return res.status(400).send('<h1>Username and password are required</h1>');
   const query = `
     INSERT INTO users (username, password, admin)
     VALUES (?, ?, 0)
   `;
+
+  connection.query(query, [username, sha256(escape(password)).toString()], (error, results) => {
+    if (error) {
+      return res.status(500).send('Error creating user');
+    }
   
-  connection.query(query, [username, sha256(escape(password)).toString()], () => {
+    const user_id = results.insertId +1;
+    req.session.user_id = user_id;
+    if (req.file) {
+      req.file.filename = user_id + path.extname(req.file.originalname);
+    }
     res.redirect('/users');
   });
 });
+
+
+app.get("/tasks", (req, res) => {
+  if (!req.session.logged) return res.redirect("/");
+  if (!req.session.isAdmin) return res.redirect("/");
+
+  connection.query("SELECT * FROM users", (error, users) => {
+    if (error) throw error;
+
+    connection.query("SELECT tasks.id, tasks.task FROM tasks ORDER BY tasks.id", (error, result) => {
+      if (error) throw error;
+      
+      const tasks = result.map(row => {
+        let task = null;
+        try {
+          task = JSON.parse(row.task);
+        } catch (err) {
+          console.error(`Failed to parse JSON data for task ${row.id}: ${row.task}`);
+        }
+        return { id: row.id, description: task ? task.description : '' };
+      });
+      
+      res.render("tasks", {
+        users: users,
+        tasks: tasks,
+        name: req.session.user,
+        perms: 'Administrator'
+      });
+    });
+  });
+});
+
+
+app.post("/tasks/:id/complete", (req, res) => {
+  const taskId = req.params.id;
+
+  connection.query('DELETE FROM tasks WHERE id = ?', [taskId], () => {
+    res.redirect(`/users`);
+  })
+  ;
+});
+
+
+
+   app.post("/tasks", (req, res) => {
+    if (!req.session.logged) return res.redirect("/");
+    if (!req.session.isAdmin) return res.redirect("/");
+   
+    const task = { description: req.body.task };
+      connection.query('INSERT INTO tasks (task) VALUES (?)', [JSON.stringify(task)], (error, result) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).send("Error inserting task");
+      }
+      res.redirect("/tasks");
+    });
+  });
+  
+
+  app.get("/logout", (req, res) => {
+    req.session.logged = false;
+    req.session.user = null;
+    req.session.isAdmin = false;
+    return res.redirect("/");
+  });
+  
+
 app.listen(3000);
